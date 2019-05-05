3c9c186b7b718ea0f02702d79f7cc6cab15f5502
bigbird231 <396016240@qq.com>
2019-05-05 09:03:17 +0800

    Add files via upload

---
+++ JavaScript/TailRecursion/fibonacci.js
@@ -0,0 +1,29 @@
+(function(){
+    //裴波那契数列
+    function fibonacci(n){
+        if(n===1){
+            return 1;
+        }
+        if(n===2){
+            return 1;
+        }
+        let result=fibonacci(n-1)+fibonacci(n-2);
+        return result;
+    }
+
+    //尾递归优化版本
+    function tailFibo(n,prev,next){
+        if(n===0){
+            return next;
+        }
+        if(next===undefined){
+            prev=0;
+            next=1;
+        }else{
+            let temp=next;
+            next=temp+prev;
+            prev=temp;
+        }
+        return tailFibo(n-1,prev,next);
+    }
+})();
